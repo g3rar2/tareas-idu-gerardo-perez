8b6fc3a50e4f34673345d1a81b88f291158fd349
g3rar2 <113952421+g3rar2@users.noreply.github.com>
2025-03-16 16:16:08 -0600

    agregando items 3 al 7

--- js/tailwind.config.js
+++ js/tailwind.config.js
@@ -3,7 +3,22 @@ tailwind.config = {
         extend: {
             colors: {
                 brand:"#FF007F",
-            }
+            },
+            spacing:{
+                72:"6.25rem",
+            },
+            width: {
+                'custom-w': '300px', // Define un ancho personalizado
+            },
+            minWidth: {
+                'custom-min-w': '200px', // Define un ancho mínimo personalizado
+            },
+            minHeight: {
+                'custom-min-h': '100px', // Define una altura mínima personalizada
+            },
+            screens: {
+                'xs': '320px', // Define el nuevo breakpoint "xs"
+            },
         }
     }
 }
